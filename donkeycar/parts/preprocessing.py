5a25e6a513d73189363f79993e5db7fb277da25b
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-05-16 08:12:23 +0300

    Added support for crop area

--- donkeycar/parts/preprocessing.py
+++ donkeycar/parts/preprocessing.py
@@ -1,18 +1,19 @@
 import numpy as np
 import time
 import cv2
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 from PIL import Image
 
 
 class PreProcessor:
-    def __init__(self, kernel_size=9, blend_colors=True, canny_min=100, canny_max=200):
+    def __init__(self, kernel_size=9, blend_colors=True, canny_min=100, canny_max=200, crop_area=None):
         #We could initialize some variables based on a single snapshot, fex size, thresholds, etc
         self.morph_kernel = np.ones((kernel_size, kernel_size), np.uint8)
         self.canny_min = canny_min
         self.canny_max = canny_max
         self.blend_colors = blend_colors
+        self.crop_area = crop_area
 
     def run(self, img_arr):
         #TODO add cropping before edge detect
@@ -29,6 +30,14 @@ class PreProcessor:
         else:
             output = mask
 
+        if self.crop_area:
+            top = self.crop_area[0][0]
+            bottom = self.crop_area[0][1]
+            left = self.crop_area[1][0]
+            right = self.crop_area[1][1]
+
+            output = output[top:bottom, left:right]
+
         end = time.time()
         print("Elapsed: ", (end - start)*1000, " ms")
 
@@ -40,6 +49,12 @@ class PreProcessor:
     def shutdown(self):
         pass
 
+    def set_crop_area(self, crop_area):
+        self.crop_area = crop_area
+
+    # Let's use the same convention as in Keras.Cropping2D
+    def crop_area(top_crop, bottom_crop, left_crop, right_crop, width, height):
+        return ((top_crop, height - bottom_crop), (left_crop, width - right_crop))
 
 class Tester:
 
@@ -56,10 +71,17 @@ class Tester:
         saveable = Image.fromarray(np.uint8(processed))
         saveable.save(new_path)
 
-    '''
+
     def process_and_plot(self, img_path):
         img = Image.open(img_path)
         arr = np.array(img)
+
+        width, height = img.size
+        print("w: ", width, " h:", height)
+        #crop_area = PreProcessor.crop_area(20, 40, 00, 0, width, height)
+        crop_area = PreProcessor.crop_area(50, 0, 0, 0, width, height)
+        self.processor.set_crop_area(crop_area)
+
         processed = self.processor.run(arr)
 
         plt.subplot(2, 2, 1), plt.imshow(arr, cmap='gray')
@@ -69,7 +91,7 @@ class Tester:
         plt.title('Processed Image'), plt.xticks([]), plt.yticks([])
 
         plt.show()
-    '''
+
 
 
 if __name__ == '__main__':
@@ -79,5 +101,5 @@ if __name__ == '__main__':
     img_path = '/Users/mpaa/donkey-data/data/data-7th/7th-set1/1164_cam-image_array_.jpg'
 
     test = Tester()
-    test.process_and_save(img_path, "./test.jpg")
-    #test.process_and_plot(img_path)
+    #test.process_and_save(img_path, "./test.jpg")
+    test.process_and_plot(img_path)
