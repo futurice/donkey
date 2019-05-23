42da9733d8ff72af3cd2b37fb4720f264c130214
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-05-23 18:42:24 +0300

    Fix package import

--- donkeycar/management/base.py
+++ donkeycar/management/base.py
@@ -7,7 +7,7 @@ import argparse
 
 import donkeycar as dk
 from donkeycar.parts.datastore import Tub
-from .tub import TubManager
+from donkeycar.management.tub import TubManager
 
 
 PACKAGE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -259,7 +259,7 @@ class Sim(BaseCommand):
         if args.type == "categorical":
             kl = KerasCategorical()
         elif args.type == "linear":
-            kl = KerasLinear()
+            kl = KerasLinear(cfg)
         else:
             print("didn't recognice type:", args.type)
             return
@@ -381,7 +381,7 @@ class ShowPredictionPlots(BaseCommand):
         if model_type == "categorical":
             model = KerasCategorical()
         elif model_type == "linear":
-            model = KerasLinear()
+            model = KerasLinear(cfg)
 
         model.load(model_path)
 
